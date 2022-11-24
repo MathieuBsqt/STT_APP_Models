ac97f96c7f10b1c8b0d1620b6f28d2a8c2709e27
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2022-11-24 11:38:46 +0100

    Add files via upload

--- download_models.py
+++ download_models.py
@@ -15,17 +15,19 @@ def load_models():
     pickle.dump(summarizer, open("/workspace/speech_to_text_app_models/summarizer.sav", 'wb'))
 
     # 3 - Other English Speech to Text Model
-    STT_tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
-    STT_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
+    model_name = "facebook/wav2vec2-large-960h-lv60-self"
+    STT_tokenizer = Wav2Vec2Tokenizer.from_pretrained(model_name)
+    STT_model = Wav2Vec2ForCTC.from_pretrained(model_name)
 
     pickle.dump(STT_model, open("/workspace/speech_to_text_app_models/STT_model2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
     pickle.dump(STT_tokenizer, open("/workspace/speech_to_text_app_models/STT_tokenizer2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
 
     # 4 - Auto Punctuation Model
-    T5_tokenizer = T5Tokenizer.from_pretrained("flexudy/t5-small-wav2vec2-grammar-fixer")
-    T5_model = T5ForConditionalGeneration.from_pretrained("flexudy/t5-small-wav2vec2-grammar-fixer")
+    model_name = "flexudy/t5-small-wav2vec2-grammar-fixer"
+    T5_tokenizer = T5Tokenizer.from_pretrained(model_name)
+    T5_model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-    pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/T5_tokenizer.sav", 'wb'))
+    pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/spiece.model", 'wb'))
     pickle.dump(T5_model, open("/workspace/speech_to_text_app_models/T5_model.sav", 'wb'))
 
     return 0
