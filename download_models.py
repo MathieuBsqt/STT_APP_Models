624fa6ca779055cdf70a1823a584805374f75ac5
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2022-11-29 11:32:09 +0100

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,5 +1,6 @@
 from transformers import pipeline, Wav2Vec2Tokenizer, Wav2Vec2ForCTC, T5Tokenizer, T5ForConditionalGeneration, HubertForCTC, Wav2Vec2Processor
 import pickle
+import torch 
 
 def load_models():
     # 1 - English Speech to Text Model
@@ -26,9 +27,13 @@ def load_models():
     model_name = "flexudy/t5-small-wav2vec2-grammar-fixer"
     T5_tokenizer = T5Tokenizer.from_pretrained(model_name)
     T5_model = T5ForConditionalGeneration.from_pretrained(model_name)
-
-    pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/spiece.model", 'wb'))
-    pickle.dump(T5_model, open("/workspace/speech_to_text_app_models/T5_model.sav", 'wb'))
+    
+    # (Here T5 Tokenizer can't be saved & loaded with pickle, as pickle serializes this model so it creates a local path in our
+    # object, which results in errors when app is not deployed locally anymore. We are going to use torch to save the model
+    torch.save(T5_tokenizer, "/workspace/speech_to_text_app_models/T5_tokenizer.sav")
+    torch.save(T5_model, "/workspace/speech_to_text_app_models/T5_model.sav")
+    #pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/spiece.model", 'wb'))
+    #pickle.dump(T5_model, open("/workspace/speech_to_text_app_models/T5_model.sav", 'wb'))
     
     # 5 - Diarization model - Can't be saved anymore since pyannote.audio v2
    
