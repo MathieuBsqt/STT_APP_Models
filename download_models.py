0b206a026111cb048f6abd363fac944fe48b0115
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2022-12-02 10:31:43 +0100

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,10 +1,7 @@
 from transformers import pipeline, Wav2Vec2Tokenizer, Wav2Vec2ForCTC, T5Tokenizer, T5ForConditionalGeneration, HubertForCTC, Wav2Vec2Processor
 import pickle
 import torch 
-import os
 
-if not os.path.exists("../workspace"):
-    os.makedirs("../workspace")
         
 def load_models():
     # 1 - English Speech to Text Model
